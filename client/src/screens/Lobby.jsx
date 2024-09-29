355cfa10812b0db83488a1d43f6263450cda7d4b
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-09-29 22:14:04 +0545

    tailwind problem sloved and new login screen made

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -33,31 +33,51 @@ function Lobby() {
   }, [socket, handleJoinRoom]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
-      <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <br />
-        <label htmlFor="room">Room ID</label>
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <br />
-        <br />
-        <button> JOIN </button>
-      </form>
+    <div className="max-w-2xl mx-auto">
+      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm m-auto mt-20 p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form onSubmit={handleSubmitForm} className="space-y-6">
+          <h1 className="text-xl font-medium text-gray-900 dark:text-white text-center">
+            LOBBY
+          </h1>
+          <div>
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              Email ID
+            </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="name@company.com"
+              required=""
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="room"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              ROOM ID
+            </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required=""
+              type="text"
+              id="room"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+          </div>
+          <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {" "}
+            JOIN{" "}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
