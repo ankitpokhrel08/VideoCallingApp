8b8804ddec53996b3f437fbf5f201e6a8ce48fb5
Ankit Kisi <kisi7ankit@gmail.com>
2024-09-29 21:32:08 +0545

    tailwind problem fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import SocketProvider from "./context/SocketProvider.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -33,30 +33,38 @@ function Lobby() {
   }, [socket, handleJoinRoom]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
-      <h1 className="text-3xl font-bold underline">Lobby</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold underline mb-10 mt-10">Lobby</h1>
+
       <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID</label>
+        <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+          Email ID
+        </label>
         <input
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
         <br />
         <br />
-        <label htmlFor="room">Room ID</label>
+        <label htmlFor="room" className="block text-gray-700 font-medium mb-2">
+          Room ID
+        </label>
         <input
           type="text"
           id="room"
           value={room}
           onChange={(e) => setRoom(e.target.value)}
+          className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
         <br />
         <br />
-        <button> JOIN </button>
+        <button className="bg-blue-500 text-white font-medium px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 hover:bg-blue-600">
+          {" "}
+          JOIN{" "}
+        </button>
       </form>
     </div>
   );
